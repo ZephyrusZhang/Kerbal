74fe258fed688409743897900302f463d397a3f9
Bill Huang <bill.huang2001@gmail.com>
2023-03-22 11:18:03 +0800

    add eex and crypto

--- kerbal-backend/mix.exs
+++ kerbal-backend/mix.exs
@@ -19,7 +19,7 @@ defmodule Kerbal.MixProject do
   def application do
     [
       mod: {Kerbal.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :eex, :crypto]
     ]
   end
 
