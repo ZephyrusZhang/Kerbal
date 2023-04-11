99ea9c8d3a4ea8e20c5f247018645d94249309a8
Bill Huang <bill.huang2001@gmail.com>
2023-04-11 17:32:59 +0800

    add auto clustering

--- tracking_station/mix.exs
+++ tracking_station/mix.exs
@@ -25,7 +25,9 @@ defmodule TrackingStation.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:rustler, "~> 0.27.0"},
-      {:uuid, "~> 1.1"}
+      {:uuid, "~> 1.1"},
+      {:mnesiac, "~> 0.3"},
+      {:libcluster, "~> 3.3"}
     ]
   end
 end
