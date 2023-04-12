f7711009e44a8520a5e725b1e03dded2dc9d96dd
Bill Huang <bill.huang2001@gmail.com>
2023-04-12 20:44:01 +0800

    add xml config
    update xml config

--- tracking_station/lib/tracking_station/scheduler/libvirt_config.ex
+++ tracking_station/lib/tracking_station/scheduler/libvirt_config.ex
@@ -5,13 +5,35 @@ defmodule TrackingStation.Scheduler.LibvirtConfig do
     :def,
     :base_config,
     "lib/tracking_station/scheduler/templates/base_config.eex",
-    [:name, :uuid, :cpu_count, :ram_size, :gpu_passthrough_config]
+    [
+      :name,
+      :uuid,
+      :cpu_count,
+      :ram_size,
+      :disk_config,
+      :iso_config,
+      :gpu_passthrough_config
+    ]
+  )
+
+  EEx.function_from_file(
+    :def,
+    :disk_config,
+    "lib/tracking_station/scheduler/templates/disk_config.eex",
+    [:disk_path]
+  )
+
+  EEx.function_from_file(
+    :def,
+    :iso_config,
+    "lib/tracking_station/scheduler/templates/iso_config.eex",
+    [:iso_path]
   )
 
   EEx.function_from_file(
     :def,
     :gpu_passthrough,
     "lib/tracking_station/scheduler/templates/gpu_passthrough.eex",
-    []
+    [:bus, :slot, :function]
   )
 end
