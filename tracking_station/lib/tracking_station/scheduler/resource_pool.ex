d3e1a28c21dc61e74080492583a7c87dd2ce7343
Bill Huang <bill.huang2001@gmail.com>
2023-04-12 21:03:35 +0800

    add resource pool
    config disk and iso

--- tracking_station/lib/tracking_station/scheduler/resource_pool.ex
+++ tracking_station/lib/tracking_station/scheduler/resource_pool.ex
@@ -1,22 +1,78 @@
+defmodule TrackingStation.Scheduler.GPUSpec do
+  defstruct [:gpu_id, :name, :vram_size]
+end
+
 defmodule TrackingStation.Scheduler.ResourceSpec do
   defstruct [:cpu_count, :ram_size, gpu_count: 0, gpu_list: []]
 end
 
+defmodule TrackingStation.Scheduler.ResourceRequest do
+  defstruct node_id: nil, cpu_count: 0, ram_size: 0, gpu_name: 0, vram_size: 0, gpu_count: 0
+end
+
+defmodule TrackingStation.Scheduler.Domain do
+  defstruct [:node_id, :domain_id, :base_img, :img, gpu_count: 0, gpu_list: []]
+end
+
 defmodule TrackingStation.Scheduler.ResourcePool do
+  alias TrackingStation.Storage.LocalStorage
   alias :mnesia, as: Mnesia
+  alias TrackingStation.Scheduler.LibvirtConfig
   alias TrackingStation.Scheduler.ResourceSpec
+  alias TrackingStation.Libvirt
 
   def init() do
-    Mnesia.create_schema([])
-    Mnesia.start()
+    gpu_ids = Application.get_env(:tracking_station, :gpu_ids)
+    valid_gpu_ids = Libvirt.valid_gpu_resource(gpu_ids)
+    IO.inspect(gpu_ids)
+    IO.inspect(valid_gpu_ids)
+    cpu_and_mem = TrackingStation.Libvirt.get_resources()
+    {cpu_and_mem, valid_gpu_ids}
+  end
 
-    case Mnesia.create_table(Scheduler.ResourcePool,
-           attributes: [:node_id, :cpu_count, :ram_size, :gpu_count, :gpu_list],
-           index: [:node_id, :cpu_count, :ram_size, :gpu_count]
-         ) do
-      {:atomic, :ok} -> nil
-      {:already_exists, _table} -> nil
+  def create_vm(node, %{cpu_count: cpu_count, ram_size: ram_size, gpus: gpus})
+      when node == node() do
+    disk_config = LibvirtConfig.disk_config(LocalStorage.allocate_disk())
+    iso_config = LibvirtConfig.iso_config(LocalStorage.get_installation_image())
+
+    gpu_passthrough =
+      gpus
+      |> Enum.map(fn %{bus: bus, slot: slot, function: function} ->
+        LibvirtConfig.gpu_passthrough(bus, slot, function)
+      end)
+      |> Enum.join("\n")
+
+    uuid = UUID.uuid1()
+
+    xml_config =
+      LibvirtConfig.base_config(
+        uuid,
+        uuid,
+        cpu_count,
+        ram_size,
+        disk_config,
+        iso_config,
+        gpu_passthrough
+      )
+
+    case Libvirt.create_vm_from_xml(xml_config) do
+      {:ok, domain_id} ->
+        # register domain here
+        {:ok, domain_id}
+      {:error, reason} ->
+        # clean up mnesia here
+        IO.inspect(reason)
     end
+  end
+
+  def create_vm(node, resources) when node != node() do
+    task = Task.Supervisor.async(
+      {TrackingStation.Scheduler.TaskSupervisor, node},
+      TrackingStation.Scheduler.ResourcePool,
+      :create_vm,
+      [node, resources]
+    )
+    Task.await(task)
   end
 
   @spec init_local_resource(ResourceSpec) :: :ok | {:error, String.t()}
@@ -32,27 +88,70 @@ defmodule TrackingStation.Scheduler.ResourcePool do
     end
   end
 
-  def lookup_resource(resource_spec) do
-    match_head = %{
+  def lookup_resource(resource_req) do
+    alive_nodes = Node.list()
+
+    query_node_match_head = %{
       node_id: :"$1",
       cpu_count: :"$2",
-      ram_size: :"$3",
-      gpu_count: :"$4",
-      gpu_list: :"$5"
+      ram_size: :"$3"
     }
 
-    guard = [
-      {:">=", :"$1", resource_spec.cpu_count},
-      {:">=", :"$2", resource_spec.ram_size},
-      {:">=", :"$3", resource_spec.gpu_count}
+    query_node_guard = [
+      {:>=, :"$2", resource_req.cpu_count},
+      {:>=, :"$2", resource_req.ram_size}
     ]
 
-    result = [:"$1", :"$5"]
+    query_node_guard =
+      if resource_req.node_id != nil do
+        [query_node_guard | {:==, :"$1", resource_req.node_id}]
+      else
+        query_node_guard
+      end
+
+    query_node_result = [:"$1"]
+
+    query_gpu_match_head = %{
+      node_id: :"$1",
+      name: :"$2",
+      vram_size: :"$3",
+      free?: :"$4",
+      online?: :"$5"
+    }
+
+    query_gpu_guard = [
+      {:>=, :"$3", resource_req.cpu_count},
+      {:==, :"$4", true},
+      {:==, :"$5", true}
+    ]
+
+    query_gpu_guard =
+      if resource_req.gpu_name != nil do
+        [query_gpu_guard | {:==, :"$2", resource_req.gpu_name}]
+      else
+        query_gpu_guard
+      end
+
+    query_gpu_result = [:"$1", :"$2", :"$3"]
 
     case Mnesia.transaction(fn ->
-      Mnesia.select(Scheduler.ResourcePool, [match_head, guard, result])
-    end) do
-      {:atomic, available_resources} -> available_resources
+           available_nodes =
+             Mnesia.select(Scheduler.ResourcePool, [
+               query_node_match_head,
+               query_node_guard,
+               query_node_result
+             ])
+
+           available_gpus =
+             Mnesia.select(Scheduler.GPUStatus, [
+               query_gpu_match_head,
+               query_gpu_guard,
+               query_gpu_result
+             ])
+
+           {available_nodes, available_gpus}
+         end) do
+      {:atomic, {available_nodes, available_gpus}} -> []
       _ -> []
     end
   end
