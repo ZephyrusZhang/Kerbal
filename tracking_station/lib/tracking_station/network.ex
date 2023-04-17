007871acfe915bc527916bd5453e0cc1173e0492
Bill Huang <bill.huang2001@gmail.com>
2023-04-17 10:59:33 +0800

    allocate port
    fix bug

--- tracking_station/lib/tracking_station/network.ex
+++ tracking_station/lib/tracking_station/network.ex
@@ -3,15 +3,54 @@ defmodule TrackingStation.Network do
   TrackingStation.Network uses Open vSwitch to build
   inter-VM network and controls how VMs connect to the internet.
   """
+  use GenServer
   alias TrackingStation.Libvirt
 
-  def init() do
+  @impl true
+  def init(
+        spice_reserved: spice_reserved,
+        tcp_port_range: tcp_port_range,
+        udp_port_range: udp_port_range
+      ) do
     # Start the default network (NAT)
     case Libvirt.start_network("default") do
       {:ok, _} -> :ok
       {:already_active, _} -> :ok
       _ -> raise "failed to start default network"
     end
+
+    # Start open vswitch for overlay network
+    System.cmd("sudo", ["ovs-vsctl", "add-br", "ovsbr0"])
+
+    %{
+      spice_reserved: Enum.to_list(spice_reserved),
+      tcp_ports: Enum.to_list(tcp_port_range),
+      udp_ports: Enum.to_list(udp_port_range)
+    }
+  end
+
+  def allocate_spice_port() do
+    GenServer.call(TrackingStation.Network, {:allocate_port, :spice_reserved})
+  end
+
+  def allocate_tcp_port() do
+    GenServer.call(TrackingStation.Network, {:allocate_port, :tcp_ports})
+  end
+
+  def allocate_udp_port() do
+    GenServer.call(TrackingStation.Network, {:allocate_port, :tcp_ports})
+  end
+
+  def free_spice_port(port) do
+    GenServer.call(TrackingStation.Network, {:free_port, :spice_reserved, port})
+  end
+
+  def free_tcp_port(port) do
+    GenServer.call(TrackingStation.Network, {:free_port, :tcp_ports, port})
+  end
+
+  def free_udp_port(port) do
+    GenServer.call(TrackingStation.Network, {:free_port, :udp_ports, port})
   end
 
   # TODO
@@ -86,5 +125,23 @@ defmodule TrackingStation.Network do
         "--to",
         "#{guest_ip}:#{guest_port}"
       ])
+  end
+
+  @impl true
+  def handle_call({:allocate_port, pool_name}, _from, state) do
+    pool = Map.get(state, pool_name)
+
+    case pool do
+      [port | rest] ->
+        {:reply, {:ok, port}, Map.replace!(state, pool_name, rest)}
+
+      [] ->
+        {:reply, {:error, :no_free_port}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:free_port, pool_name, port}, _from, state) do
+    Map.get_and_update(state, pool_name, fn pool -> [port | pool] end)
   end
 end
