a4df9e29b36d4a7e913339bfde7f4bb0c876a673
Bill Huang <bill.huang2001@gmail.com>
2023-05-16 11:05:09 +0800

    fix jwt typo

--- kerbal-backend/lib/kerbal_web/jwt_token.ex
+++ kerbal-backend/lib/kerbal_web/jwt_token.ex
@@ -1,15 +1,16 @@
 defmodule KerbalWeb.JWTToken do
   use Joken.Config
+  require Logger
   import Plug.Conn
 
   def fetch_jwt(conn, _opts) do
-    case conn |> get_req_header("Authentication") do
+    case conn |> get_req_header("authorization") do
       [token] ->
         case KerbalWeb.JWTToken.verify_and_validate(token) do
           {:ok, claims} ->
             conn |> assign(:claims, claims)
 
-          {:error, _} ->
+          {:error, reason} ->
             conn
         end
 
