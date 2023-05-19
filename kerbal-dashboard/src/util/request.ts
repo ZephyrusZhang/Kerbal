ef1fb1491e317b75980323ad10384e4c4665c8f1
Frank <FrankssWu@outlook.com>
2023-05-19 20:40:37 +0800

    feat(AlertDialog): define toast in util/request

--- kerbal-dashboard/src/util/request.ts
+++ kerbal-dashboard/src/util/request.ts
@@ -1,10 +1,31 @@
 import axios from "axios";
 import { isToRenew } from "./jwt";
-
 const request = axios.create({
   baseURL: "http://localhost:3000/",
   timeout: 5000
-})
+}) 
+
+import { useToast } from '@chakra-ui/react'
+export const ShowToast= (toast: ReturnType<typeof useToast>, msg: string, typ: string) => {
+  toast({
+    position: 'top',
+    status: function () { // stupid js need this to assure the return data?
+      switch (typ) {
+        case 'success':
+          return 'success'
+        case 'error':
+          return 'error'
+        case 'warning':
+          return 'warning'
+        case 'info':
+          return 'info'
+        default:
+          return 'loading'
+        }}(),
+    duration: 2000,
+    description: msg
+  })
+}
 
 request.interceptors.request.use(config => {
   config.headers['Content-Type'] = 'application/json;charset=utf-8'
