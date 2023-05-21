8deed0766bf23a545ccb1384d46de9dce313e905
dkRose <nw1109225291@outlook.com>
2023-05-21 19:32:57 +0800

    feat(bug fix): Fix package, succeed get report.

--- kerbal-dashboard/jest.config.js
+++ kerbal-dashboard/jest.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    collectCoverage: true,
-    collectCoverageFrom: [
-      'src/**/*.{ts,tsx}' // 指定要收集覆盖率信息的源代码文件路径
-    ],
-    coverageReporters: [
-      'lcov', // 使用lcov格式生成报告
-      'text' // 在终端中以文本形式显示覆盖率报告
-    ],
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testRegex: '/__tests__/.*\\.(test|spec)\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
