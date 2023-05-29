c74902a929bde4bc39dbed95bc0cee0d7bc964d0
Bill Huang <bill.huang2001@gmail.com>
2023-05-29 21:26:18 +0800

    use localhost

--- kerbal-backend/config/dev.exs
+++ kerbal-backend/config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :kerbal, Kerbal.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "planet1",
+  hostname: "localhost",
   database: "kerbal_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
