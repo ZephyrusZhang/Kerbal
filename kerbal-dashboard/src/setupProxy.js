5ec0e012bef4486430900fca6a9699040e30c479
ZephyrusZhang <2996362441@qq.com>
2023-05-31 04:03:33 +0800

    change proxy server target

--- kerbal-dashboard/src/setupProxy.js
+++ kerbal-dashboard/src/setupProxy.js
@@ -1,12 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware')
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://10.16.97.70:4000/',
+      target: 'http://10.16.112.16:4000/',
       changeOrigin: true,
     })
-  );
-};
+  )
+
+  app.use(
+    '/websock',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:9000/',
+      changeOrigin: true
+    })
+  )
+}
