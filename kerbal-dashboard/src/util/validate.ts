09dd453d51d01be31bcf6b61e9a76821f15efb41
ZephyrusZhang <2996362441@qq.com>
2023-05-31 04:04:26 +0800

    feat(util): add port validator

--- kerbal-dashboard/src/util/validate.ts
+++ kerbal-dashboard/src/util/validate.ts
@@ -1,6 +1,6 @@
 import { emailRegex, passwordRegex } from "../const";
 
-export const validatePassword = (values: {password: string, passwordConfirmation: string}) => {
+export const validatePassword = (values: { password: string, passwordConfirmation: string }) => {
   const errors: { password?: string, passwordConfirmation?: string } = {}
   if (values.password === '') errors.password = 'Password is required'
   else if (!passwordRegex.test(values.password)) errors.password = 'Invalid password'
@@ -9,8 +9,15 @@ export const validatePassword = (values: {password: string, passwordConfirmation
 }
 
 export const validateEmail = (email: string) => {
-  const errors: {email?: string} = {}
+  const errors: { email?: string } = {}
   if (email === '') errors.email = 'Email is required'
   else if (!emailRegex.test(email)) errors.email = 'Email is invalid'
+  return errors
+}
+
+export const validatePort = (key: string, port: string, notnull = true) => {
+  const errors: { [key: string]: string } = {}
+  if ((port === '' || port === undefined || port === null) && notnull) errors[key] = 'Port is required'
+  else if (!(0 <= parseInt(port) && parseInt(port) <= 65535)) errors[key] = 'Port must be within range of 0 to 65535'
   return errors
 }
