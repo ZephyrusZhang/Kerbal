90836aba456dab7c1259f7a5791e037641a345af
ZephyrusZhang <2996362441@qq.com>
2023-05-31 15:38:15 +0800

    fix(DomainDashboard): unable to copy spice remote address on server

--- kerbal-dashboard/src/util/page.ts
+++ kerbal-dashboard/src/util/page.ts
@@ -1,3 +1,5 @@
+import { toast } from "./toast";
+
 export const refresh = () => {
   window.location.reload()
 }
@@ -10,4 +12,31 @@ export const refresh = () => {
  */
 export const parsePathSegments = (pathname: string) => {
   return (pathname.split('/').filter(segment => segment !== ''))
+}
+
+export const copyToClipboard = (text: string) => {
+  const textarea = document.createElement('textarea')
+  textarea.value = text
+  document.body.appendChild(textarea)
+  textarea.focus()
+  textarea.select()
+  try {
+    document.execCommand('copy')
+    toast({
+      title: 'Address Copied Successfully',
+      status: 'success',
+      duration: 2000,
+      position: 'top',
+      isClosable: true
+    })
+  } catch (err) {
+    toast({
+      title: `Unable to copy: ${err}`,
+      status: 'error',
+      duration: 2000,
+      position: 'top',
+      isClosable: true
+    })
+  }
+  document.body.removeChild(textarea)
 }
