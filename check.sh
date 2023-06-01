2aeb708e987595b28d318e50d9ef2778d52d0ddf
Frank <FrankssWu@outlook.com>
2023-06-01 11:25:05 +0800

    fix(ci): update backend check

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /nixos
-echo 'whoami && id && pwd && mix test' | nix develop ./flake.nix --show-trace --extra-experimental-features nix-command --extra-experimental-features flakes
+nix develop ./flake.nix --show-trace --extra-experimental-features nix-command --extra-experimental-features flakes
